2cf124d14796d2f014ce68d1f087f422c5d26189
Leo <58927485+LeoFeatherstone@users.noreply.github.com>
2021-01-29 20:43:30 +1100

    Add files via upload

--- simulations/convert_bd_bdsky.R
+++ simulations/convert_bd_bdsky.R
@@ -1,12 +1,7 @@
 # converting BD xml files into bdsky. Involves cutting sampling parameter into two
 
-
 names_xml <- dir(pattern='.+xml')
-names_xml <- names_xml[-grep(names_xml, pattern='pruned')]
-
 xml <- lapply(names_xml, function(x) readLines(con=x))
-
-
 bdsky_xml <- list()
 
 for (i in 1:length(names_xml)){
@@ -14,7 +9,10 @@ for (i in 1:length(names_xml)){
 	bdsky_xml[[i]] <- c(bdsky_xml[[i]], '<function spec="beast.core.util.Slice" id="one" arg="@samplingProportionPrior.t:BDskyline" index="0" count="1"/>',
 	'<function spec="beast.core.util.Slice" id="two" arg="@samplingProportionPrior.t:BDskyline" index="1" count="1"/>')
 
-	bdsky_xml[[i]] <- c(bdsky_xml[[i]], xml[[i]][(1+max(grep(xml[[i]], pattern='map>'))):grep(xml[[i]], pattern='<distribution id="BirthDeathSkySerial')-1])
+	bdsky_xml[[i]] <- c(bdsky_xml[[i]], xml[[i]][(1+max(grep(xml[[i]], pattern='map>'))):(grep(xml[[i]], pattern='<parameter id=\\"samplingProportion.t\\:BDskyline\\"')-1)])
+	bdsky_xml[[i]] <- c(bdsky_xml[[i]], '<parameter id="samplingProportion.t:BDskyline" lower="0.0" name="stateNode" upper="1.0" dimension="2">0.1</parameter>')
+	bdsky_xml[[i]] <- c(bdsky_xml[[i]], xml[[i]][(grep(xml[[i]], pattern='<parameter id="becomeUninfectiousRate.t:BDskyline"')):(grep(xml[[i]], pattern='<distribution id="BirthDeathSkySerial')-1)])
+
 	bdsky_xml[[i]] <- c(bdsky_xml[[i]], '          <distribution id="BirthDeathSkySerial.t:BDskyline"
 				spec="beast.evolution.speciation.BirthDeathSkylineModel"
 				becomeUninfectiousRate="@becomeUninfectiousRate.t:BDskyline"
